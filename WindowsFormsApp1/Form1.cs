c6b51e1ebb92ae0a9ec57acd83496b1ed681d17b
engab1 <nagygabor0425@gmail.com>
2019-02-08 21:06:35 +0100

    2222

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -19,7 +19,7 @@ namespace WindowsFormsApp1
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Hahó01");
-            MessageBox.Show("Hahó0222");
+            MessageBox.Show("Hahó02222");
         }
     }
 }
